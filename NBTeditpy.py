1331cc750c5efb080b3f4ffd2ab5ab08d61a8b9f
12skoko <1458987208@qq.com>
2021-03-20 19:59:33 +0800

    update

--- NBTeditpy.py
+++ NBTeditpy.py
@@ -130,35 +130,3 @@ class NBTedit:
                                       }}
         self.setblock(coor, cb)
 
-
-stone = {'Name': 'minecraft:stone', 'tag': 'stone', 'Properties': {'variant': 'stone'}}
-dirt = {'Name': 'minecraft:dirt', 'tag': 'dirt'}
-grass = {'Name': 'minecraft:grass', 'tag': 'grass'}
-cobblestone = {'Name': 'minecraft:cobblestone', 'tag': 'cobblestone'}
-
-nn = NBTedit('12skoko', [2, 2, 2])
-
-nn.addblockskind(stone)
-nn.addblockskind(dirt)
-nn.addblockskind(grass)
-nn.addblockskind(cobblestone)
-
-nn.setblock([0, 0, 0], {'tag': 'stone'})
-nn.setblock([0, 0, 1], {'tag': 'dirt'})
-nn.setblock([0, 1, 1], {'tag': 'grass'})
-nn.setblock([1, 0, 1], {'tag': 'cobblestone'})
-
-nn.setcommandblock([1, 1, 0], 'helloworld', [0, 0, 0, 0])
-
-# print(nn.blocktags)
-nn.printNBT()
-nn.saveNBT('h4.nbt')
-#
-# tt={'variant':'563625','variant_type':0,'conditional': 1243,'conditional_type':1, 'facing': '8465654','facing_type':2}
-# for i in tt:
-#     if '_type' not in i:
-#
-#         print(i)
-#         print(tt[i])
-#         print(tt[str(i)+'_type'])
-#         print('\n\n\n')
