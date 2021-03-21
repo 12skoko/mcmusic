0728d457530384f3c4a979ed7f6f6ff397b6bb8a
12skoko <1458987208@qq.com>
2021-03-21 23:41:33 +0800

    update

--- test2.py
+++ test2.py
@@ -1,16 +1,1 @@
 
-
-
-import nbt
-
-nnn = nbt.nbt.NBTFile("4.nbt")
-
-# print(nnn["blocks"][0]['nbt']['Command'])
-
-# for i in nnn['blocks']:
-
-    # print(i)
-
-print(nnn.pretty_tree())
-
-# print(nnn)
--- test5.py
+++ test5.py
@@ -47,31 +47,32 @@ nn = NBTeditpy.NBTedit('12skoko', [200, 1, 8])
 for i, track in enumerate(mid.tracks):
 
     if i == 2:
+        k=0
         for msg in track:
             msgdict = msg.dict()
-
             flag = vaguemun(int(msgdict['time']))
-
-            if flag == 0 and 'note_on' in msgdict['type']:
+            # if k==1:
+            if flag == 0 and 'note_on' in msgdict['type'] and (k==1 or 'note_on' in msglast['type']):
                 xz[1] += 1
             else:
                 xz[0] += flag
                 xz[1] = 0
-
+            # else:
+            #     if flag == 0 and 'note_on' in msgdict['type'] :
+            #         xz[1] += 1
+            #     else:
+            #         xz[0] += flag
+            #         xz[1] = 0
             if 'note_on' in msgdict['type']:
-
                 print('\033[1;31m' + str(msg) + '\033[0m')
-
-
-
-                nn.setcommandblock([xz[0], 0, xz[1]], 'execute @p ~ ~ ~ playsound 4.'+str(msgdict['note'])+' voice @p ~ ~ ~ 0.6', [1, 0, 1, 1])
-
-
+                nn.setcommandblock([xz[0], 0, xz[1]], 'execute @p ~ ~ ~ playsound 4.'+str(msgdict['note'])+' voice @p ~ ~ ~ '+str(msgdict['velocity']/128.0), [1, 0, 1, 1])
             elif 'note_off' in msgdict['type']:
-
                 print('\033[1;34m' + str(msg) + '\033[0m')
-
+            elif msgdict['time']!=0:
+                print('\033[1;33m' + str(msg) + '\033[0m')
             else:
                 print(str(msg))
+            k=1
+            msglast=msgdict
 # nn.printNBT()
-nn.saveNBT('t1.nbt')
+nn.saveNBT('t2.nbt')
