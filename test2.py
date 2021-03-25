d53f999d06abb05ce0eba184f5a01cb540cd6bfe
12skoko <1458987208@qq.com>
2021-03-25 21:57:20 +0800

    update

--- test2.py
+++ test2.py
@@ -1,1 +1,20 @@
 
+import operator
+
+
+a=[
+
+{'note': 76, 'velocity': 71, 'chet': 9, 'time': 1},
+{'note': 45, 'velocity': 71, 'chet': 6, 'time': 4},
+{'note': 71, 'velocity': 71, 'chet': 10, 'time': 0},
+{'note': 78, 'velocity': 71, 'chet': 8, 'time': 2},
+{'note': 83, 'velocity': 71, 'chet': 6, 'time': 4},
+{'note': 52, 'velocity': 74, 'chet': 6, 'time': 10}
+    ]
+
+sorted_x = sorted(a, key=operator.itemgetter('time'))
+# sorted_x = sorted(a, key=lambda x : x['time'])
+
+
+for i in sorted_x:
+    print(i)
--- test5.py
+++ test5.py
@@ -2,6 +2,8 @@ from mido import MidiFile
 import NBTeditpy
 
 
+
+
 def vaguemun(num):
     if num <=3:
         return 0
@@ -28,14 +30,17 @@ def vaguemun(num):
         exit(65424)
 
 
+
+
 mid = MidiFile('2.mid')
 
 xz = [0,0]
 
+musicnote=[]
+nowon=[]
+tick=0
+
 # nn = NBTeditpy.NBTedit('12skoko', [200, 1, 8])
-
-
-
 
 # nn.setcommandblock([1, 1, 0], 'helloworld', [0, 0, 0, 0])
 
@@ -44,35 +49,61 @@ xz = [0,0]
 # nn.saveNBT('h4.nbt')
 
 
-for i, track in enumerate(mid.tracks):
+# for i, track in enumerate(mid.tracks):
+#
+#     if i == 2:
+#         k=0
+#         for msg in track:
+#             msgdict = msg.dict()
+#             flag = vaguemun(int(msgdict['time']))
+#             # if k==1:
+#             if flag == 0 and 'note_on' in msgdict['type'] and (k==1 or 'note_on' in msglast['type']):
+#                 xz[1] += 1
+#             else:
+#                 xz[0] += flag
+#                 xz[1] = 0
+#             # else:
+#             #     if flag == 0 and 'note_on' in msgdict['type'] :
+#             #         xz[1] += 1
+#             #     else:
+#             #         xz[0] += flag
+#             #         xz[1] = 0
+#             if 'note_on' in msgdict['type']:
+#                 print('\033[1;31m' + str(msg) + '\033[0m')
+#                 # nn.setcommandblock([xz[0], 0, xz[1]], 'execute @p ~ ~ ~ playsound 4.'+str(msgdict['note'])+' voice @p ~ ~ ~ '+str(msgdict['velocity']/128.0), [1, 0, 1, 1])
+#             elif 'note_off' in msgdict['type']:
+#                 print('\033[1;34m' + str(msg) + '\033[0m')
+#             elif msgdict['time']!=0:
+#                 print('\033[1;33m' + str(msg) + '\033[0m')
+#             else:
+#                 print(str(msg))
+#             k=1
+#             msglast=msgdict
+# # nn.printNBT()
+# # nn.saveNBT('t2.nbt')
 
+
+for i, track in enumerate(mid.tracks):
     if i == 2:
-        k=0
         for msg in track:
             msgdict = msg.dict()
             flag = vaguemun(int(msgdict['time']))
-            # if k==1:
-            if flag == 0 and 'note_on' in msgdict['type'] and (k==1 or 'note_on' in msglast['type']):
-                xz[1] += 1
-            else:
-                xz[0] += flag
-                xz[1] = 0
-            # else:
-            #     if flag == 0 and 'note_on' in msgdict['type'] :
-            #         xz[1] += 1
-            #     else:
-            #         xz[0] += flag
-            #         xz[1] = 0
+            if flag !=0:
+                tick+=flag
+
+            if 'note_off' in msgdict['type']:
+                for i in range(0,len(musicnote)):
+                    if musicnote[i]['note']==msgdict['note']:
+                        musicnoteinfo={'note':msgdict['note'],'velocity':musicnote[i]['time'],'time':tick-musicnote[i]['time']}
+                        musicnote.append(musicnoteinfo)
+                        temp=musicnote[i]
+                        nowon.remove(temp)
+                        break
+
             if 'note_on' in msgdict['type']:
-                print('\033[1;31m' + str(msg) + '\033[0m')
-                # nn.setcommandblock([xz[0], 0, xz[1]], 'execute @p ~ ~ ~ playsound 4.'+str(msgdict['note'])+' voice @p ~ ~ ~ '+str(msgdict['velocity']/128.0), [1, 0, 1, 1])
-            elif 'note_off' in msgdict['type']:
-                print('\033[1;34m' + str(msg) + '\033[0m')
-            elif msgdict['time']!=0:
-                print('\033[1;33m' + str(msg) + '\033[0m')
-            else:
-                print(str(msg))
-            k=1
-            msglast=msgdict
-# nn.printNBT()
-# nn.saveNBT('t2.nbt')
+                noteinfo={'note':msgdict['note'],'velocity':msgdict['velocity'],'time':tick}
+
+
+
+for i in musicnote:
+    print(i)
